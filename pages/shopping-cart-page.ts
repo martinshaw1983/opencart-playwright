718c5ed5375dd642cc790c96c481d90e3f0b1cfb
Martin <mr.mp.shaw.1983@gmail.com>
2026-05-20 15:11:28 +0100

    fix typo in ensureEmptyCart var

--- pages/shopping-cart-page.ts
+++ pages/shopping-cart-page.ts
@@ -62,7 +62,7 @@ export class ShoppingCartPage extends BasePage {
         }
     }
 
-    async enureEmptyCart() {
+    async ensureEmptyCart() {
                if(!(await this.cartTotal.innerText()).includes('0 item(s)')) {
                 console.log('Teardown: Removing leftover items in cart');
                 await this.page.goto('index.php?route=checkout/cart');
